ddca5e521900b0e3080be8c9d20b819bab91eced
saheba <mail-@-saheba-dot-net>
2014-06-25 22:06:19 +0200

    tutorial section 3.2.2 - added test case for about page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,8 +10,15 @@ describe "static pages" do
 
   describe "help page" do
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
     end
+  end
+
+  describe "about page" do
+  	it "should have the content 'About Us'" do
+  		visit 'static_pages/about'
+  		expect(page).to have_content('About Us')
+  	end
   end
 end
