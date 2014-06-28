e86d3550f5c83e38cfd456801e9dc1863ae5471f
saheba <mail-@-saheba-dot-net>
2014-06-28 09:42:10 +0200

    tutorial section 7.1.3 - styled minimal user profile page, added test case with factory girl

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name "shb"
+    email "shb@example.com"
+    password 'shbshb'
+    password_confirmation 'shbshb'
+  end
+end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -9,4 +9,11 @@ describe "user pages" do
     it { should have_selector('h1', text: 'Sign up') }
     it { should have_title("#{base_title} | Signup") }
   end
+  describe "profile page" do
+  	let(:user) {FactoryGirl.create(:user)}
+  	before { visit user_path(user) }  	
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
+  end
+
 end
