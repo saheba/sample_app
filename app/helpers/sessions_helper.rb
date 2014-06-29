0c312999090734d9b410957f6913e0ce8e683139
saheba <mail-@-saheba-dot-net>
2014-06-29 20:22:10 +0200

    section 8.2.3 - handling token creation during signup (cookie time :)

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,24 @@
 module SessionsHelper
+	def sign_in(user)
+		remember_token = user.new_remember_token()
+		cookies.permanent[:remember_token] = remember_token
+		# update attribute bypasses validations! (Hartl: we do this because we do not have the password?)
+		# but sign_in is called with a user object from the SessionsController containing username and password ?!
+		user.update_attribute(:remember_token, user.digest(remember_token))
+		# just necessary if we want to use sign_in without a redirect
+		# a redirect reloads the current user, a rerendering doesn't
+		@current_user = user
+	end
+
+	# def current_user=(user)
+	# 	@current_user = user
+	# end
+
+	def current_user
+		digest_token = User.digest(cookies[:remember_token])
+		return @current_user ||= User.find_by(remember_token: digest_token)
+		## this is compact style for
+		# return @current_user ? @current_user : User.find_by(...)
+		
+	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,28 +1,26 @@
 class User < ActiveRecord::Base
-	validates :name, presence: true, length: { maximum: 100 }
-	validates :password, presence: true, length: { minimum: 6 }
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
-	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
-						uniqueness: { case_sensitive: false }
-	## assigning block to callback hook
-	before_save { self.email.downcase! }
-	has_secure_password
-
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :password, presence: true, length: { minimum: 6 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+    uniqueness: { case_sensitive: false }
+  ## assigning block to callback hook
+  before_save { self.email.downcase! }
+  has_secure_password
   ## assigning block to callback hook with method reference
-	before_create :create_remember_token
+  before_create :create_remember_token
 
-	private
-
-		def new_remember_token
-			return SecureRandom.urlsafe_base64()
-		end
-
-		def digest(token)
-			return Digest::SHA1.hexdigest(token.to_s)
-		end
+  def new_remember_token
+    return SecureRandom.urlsafe_base64()
+  end
+  def digest(token)
+    return Digest::SHA1.hexdigest(token.to_s)
+  end
+  
+  private
 
 	  def create_remember_token
-	  	## could it also be: @remember_token?
-	  	self.remember_token = digest(new_remember_token())
+	    ## could it also be: @remember_token?
+	    self.remember_token = digest(new_remember_token())
 	  end
 end
