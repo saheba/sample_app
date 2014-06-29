b94fbbbaa78e8f2ccee5f8913bb91c3b2fca64ba
saheba <mail-@-saheba-dot-net>
2014-06-29 17:19:53 +0200

    section 8.1.2 - getting to now the difference between flash and flash.now using rspec example

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,15 @@ class SessionsController < ApplicationController
 	end
 
 	def create
+		user = User.find_by(email: params[:sessions][:email].downcase)
+		if user && user.authenticate(params[:sessions][:password])
+			# render profile page
+
+		else
+			# render sign in page again and show errors
+			flash[:error] = 'Invalid email/password combination'
+			render 'new'
+		end
 	end
 
 	def destroy
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -1,35 +1,40 @@
 require 'spec_helper'
 
 describe "Authentication" do
-	subject { page }
+  subject { page }
 
-  describe "sign in page" do
+  describe "sign in" do
     before { visit signin_path }
     it { should have_content('Sign in') }
     it { should have_title('Sign in') }
-  end
 
-  context "with invalid information" do
 
-  	describe "after sub" do 
+    context "with invalid information" do
+
+      describe "and after submission" do
         before { click_button "Sign in" }
         it { should have_selector('div.alert.alert-error') }
-    end
-  end
 
-  context "with valid information" do
-  	let(:user) { FactoryGirl.create(:user) }
-  	before do
-  		fill_in "Email", with: user.email.upcase
-  		fill_in "Password", with: user.password
-  		click_button "Sign in"
-  	end
-  	describe "after sub" do 
+        describe "and after visiting another page" do
+        	it { should have_no_selector('div.alert.alert-error') }
+        end
+      end
+    end
+
+    context "with valid information" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        fill_in "Email", with: user.email.upcase
+        fill_in "Password", with: user.password
+        click_button "Sign in"
+      end
+      describe "after sub" do
         before { click_button "Sign in" }
         ## parsing for content as long as we do finally know how the page layout will look like
-        it { should_not have_link('Sign in', href: signin_path) }
+        it { should have_no_link('Sign in', href: signin_path) }
         it { should have_link('Sign out', href: signout_path ) }
         it { should have_link('Profile', href: user_path(user)) }
+      end
     end
   end
 end
