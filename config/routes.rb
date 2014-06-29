da04d697c4519314e602890c12ba3d87e7af0294
saheba <mail-@-saheba-dot-net>
2014-06-29 15:23:33 +0200

    section 8.1.1 - minimal sign in page + test case

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,20 @@
 SampleApp::Application.routes.draw do
   # endosing app with all RESTful user actions
   resources :users
+  # no need to show or edit sessions
+  resources :sessions, only: [:new, :create, :destroy]
   #get "static_pages/home"
   #get "static_pages/help"
   #get "static_pages/about"
   #get "static_pages/contact"
   root 'static_pages#home'
+  ## additional REST urls to perform RESTful actions
   match '/help', to: 'static_pages#help', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
   match '/signup', to: 'users#new', via: 'get'
+  match '/signin', to: 'sessions#new', via: 'get'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
