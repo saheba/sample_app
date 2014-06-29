08642cd7671c8decae7267b1a16370dafecb6b89
saheba <mail-@-saheba-dot-net>
2014-06-29 16:35:35 +0200

    section 8.1.2 - defined test cases for successful and unsuccessful login attempt

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -8,4 +8,23 @@ describe "Authentication" do
     it { should have_content('Sign in') }
     it { should have_title('Sign in') }
   end
+
+  context "with invalid information" do
+
+  	describe "after sub" do 
+        before { click_button "Sign in" }
+        it { should have_selector('div.alert.alert-error') }
+    end
+  end
+
+  context "with valid information" do
+
+  	describe "after sub" do 
+        before { click_button "Sign in" }
+        ## parsing for content as long as we do finally know how the page layout will look like
+        it { should_not have_content('Sign in') }
+        it { should have_content('Sign out') }
+        it { should have_content('Profile') }
+    end
+  end
 end
