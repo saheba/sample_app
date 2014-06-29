1038d6bc339f19092325d4e3badf553fcf940866
saheba <mail-@-saheba-dot-net>
2014-06-29 17:59:37 +0200

    section 8.2.1 - added remember token attribute required to impl SessionsController.sign_in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,8 @@ class SessionsController < ApplicationController
 		user = User.find_by(email: params[:sessions][:email].downcase)
 		if user && user.authenticate(params[:sessions][:password])
 			# render profile page
-
+			sign_in user
+			redirect_to user
 		else
 			# render sign in page again and show errors
 			flash.now[:error] = 'Invalid email/password combination'
