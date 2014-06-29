2c435381174b886507db3dd181f01b6aa5994c1a
saheba <mail-@-saheba-dot-net>
2014-06-29 22:47:21 +0200

    8.3.2 - cucumber scenarios and step definitions with factory girl integration :)

---
+++ features/step_definitions/authentication_steps.rb
@@ -0,0 +1,35 @@
+require 'factory_girl'
+
+Given /^a user visits the signin page$/ do
+  # subject { page } ## does not work in cucumber
+
+  # plain capybara (included by default in cc step files like this one)
+  visit signin_path
+end
+
+When /^they submit invalid signin information$/ do
+  click_button 'Sign in'
+end
+
+Then /^they should see an error message$/ do
+  expect(page).to have_selector('div.alert.alert-error')
+end
+
+
+Given /^the user has an account$/ do
+	@user = FactoryGirl.create(:user)
+end
+
+When /^they submit valid signin information$/ do
+	fill_in "Email", with: @user.email
+	fill_in "Password", with: @user.password
+	click_button 'Sign in'
+end
+
+Then /^they should see their profile page$/ do
+	expect(page).to have_title(@user.name)
+end
+
+And /^they should see a signout link$/ do
+	expect(page).to have_link('Sign out', href: signout_path)
+end
