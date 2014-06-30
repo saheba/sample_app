71880e8113cc10ab403361cafeb7e484ce4f8924
saheba <mail-@-saheba-dot-net>
2014-06-30 14:34:24 +0200

    9.2.2 - test cases to ensure users can only update their own profiles

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -60,5 +60,35 @@ describe "Authentication" do
 
       end
     end
+
+    describe "as wrong user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
+
+      describe "simulated by capybara" do
+        before { sign_in user }
+        
+        describe "trying to edit other user profile" do
+          before { visit edit_user_path(wrong_user)}
+
+           it { should have_title(full_title('Edit')) }
+           it { should have_button('Save changes') }
+        end
+      end
+
+      describe "using low-level http requests" do
+        before { sign_in user, no_capybara: true }
+
+        describe "submitting a GET request to the Users#edit action" do
+          before { get edit_user_path(wrong_user) }
+          specify { expect(response.body).not_to match(full_title('Edit')) }
+          specify { expect(response).to redirect_to(root_url) }
+        end
+        describe "submitting a PATCH request" do
+          before { patch edit_user_path(wrong_user) }          
+          specify { expect(response).to redirect_to(root_url) }        
+        end
+      end
+    end
   end
 end
