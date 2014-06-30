934b8b0dae30a4ecd34a0a06e5767c174663cfe8
saheba <mail-@-saheba-dot-net>
2014-06-30 12:41:25 +0200

    section 9.1.1 - refactored user profile form into partial, added test cases and view for edit user feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,11 @@ class UsersController < ApplicationController
   end
 
   def show
-  	@user = User.find(params[:id])  	
+  	@user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
   end
 
   def create
