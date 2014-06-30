eb15b7d4074791d451075cf5fc75229fabfdb948
saheba <mail-@-saheba-dot-net>
2014-06-30 13:53:52 +0200

    section 9.1 - completed edit user feature (without auth)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,17 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # handle successful update
+      flash[:success] ="Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   def create
   	@user = User.new(user_params)
   	if @user.save
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -63,7 +63,10 @@ describe "user pages" do
 
   describe "edit" do
     let(:user) { FactoryGirl.create(:user) }
-    before { visit edit_user_path(user) }
+    before do
+      sign_in user
+      visit edit_user_path(user)
+    end
 
     describe "page" do
       it { should have_content('Update your profile') }
@@ -75,6 +78,24 @@ describe "user pages" do
       before { click_button "Save changes" }
 
       it { should have_content('error') }
+    end
+
+    describe "with valid information" do
+      let(:new_name) { "New Name" }
+      let(:new_email) { "new@example.com" }
+      before do
+        fill_in "Name", with: new_name
+        fill_in "Email", with: new_email
+        fill_in "Password", with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+        click_button "Save changes"
+      end
+
+        it { should have_link('Sign out', href: signout_path) }        
+        it { should have_title(full_title(new_name)) }
+        it { should have_selector('div.alert.alert-success', 'Welcome :)') }
+        specify { expect(user.reload.name).to eq new_name }
+        specify { expect(user.reload.email).to eq new_email }
     end
   end
 end
