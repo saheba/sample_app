dadcd2b7c4a7525f8450829147c9323dc6a53ba6
saheba <mail-@-saheba-dot-net>
2014-07-04 13:16:17 +0200

    section 10.1.2 - micropost regression test added

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -12,4 +12,9 @@ describe Micropost do
   it { should respond_to(:user_id)}
   it { should respond_to(:created_at)}
   it { should respond_to(:updated_at)}
+
+  describe "when user_id is not valid" do
+    before { @post.user_id = nil }
+    it { should_not be_valid }
+  end
 end
